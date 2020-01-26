046380a362a60b6caeef462e6f788abb4ef5a815
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2020-01-26 18:00:36 +0100

    extend mmu

--- include/mmu.h
+++ include/mmu.h
@@ -5,15 +5,17 @@
 class MMU
 {
 public:
-	MMU(Cartridge *_) : cartridge(_) {}
+	MMU() {}
 	~MMU();
 
 public:
+	u8 ROM[256] = {0};
 	Cartridge *cartridge;
 
 	template <typename T>
 	u8 read(u16);
-
 	template <typename T>
 	void write(u16, u8);
+
+	void connectCartridge(Cartridge*);
 };
