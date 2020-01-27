4c850df559092659be7aec54061393e05af60f76
Ramiz Polić <32913827+fhivemind@users.noreply.github.com>
2020-01-27 16:13:51 +0100

    mmu ppu handling

--- include/mmu.h
+++ include/mmu.h
@@ -4,18 +4,27 @@
 
 class MMU
 {
+private:
+	// CPU related
+	u8 ROM[256] = { 0 };
+
+	// PPU related
+	u8 PatternTable[2][2048];
+	u8 NameTable[2][1024];
+	u8 PaletteTable[32];
+
+	// Cartridge related
+	Cartridge* cartridge;
+
 public:
 	MMU() {}
 	~MMU();
 
 public:
-	u8 ROM[256] = {0};
-	Cartridge *cartridge;
-
 	template <typename T>
 	u8 read(u16);
 	template <typename T>
 	void write(u16, u8);
+	inline void connectCartridge(Cartridge*);
 
-	void connectCartridge(Cartridge*);
 };
--- src/mmu.cpp
+++ src/mmu.cpp
@@ -1,1 +1,56 @@
 #include "mmu.h"
+
+MMU::MMU()
+{
+}
+
+MMU::~MMU()
+{
+}
+
+template<typename T>
+inline u8 MMU::read(u16 addr)
+{
+	u8 data{ 0 };
+	if constexpr (std::is_same_v<T, CPU>) {
+		if (cartridge->read<CPU>(addr, data)) {}
+		else if (addr >= 0x0000 && addr <= 0x1FFF)
+		{
+			data = ROM[addr & 0x07FF];
+		}
+		else if (addr >= 0x2000 && addr <= 0x3FFF)
+		{
+			// PPU to CPU Read
+		}
+	}
+	else if constexpr (std::is_same_v<T, PPU>) {
+		addr &= 0x3FFF;
+		cartridge->read<PPU>(addr, data);
+	}
+	return data;
+}
+
+template<typename T>
+inline void MMU::write(u16 addr, u8 data)
+{
+	if constexpr (std::is_same_v<T, CPU>) {
+		if (cartridge->write<CPU>(addr, data)) {}
+		else if (addr >= 0x0000 && addr <= 0x1FFF)
+		{
+			ROM[addr & 0x07FF] = data;
+		}
+		else if (addr >= 0x2000 && addr <= 0x3FFF)
+		{
+			// PPU to CPU Write
+		}
+	}
+	else if constexpr (std::is_same_v<T, PPU>) {
+		addr &= 0x3FFF;
+		cartridge->write<PPU>(addr, data);
+	}
+}
+
+inline void MMU::connectCartridge(Cartridge* _cartridge)
+{
+	cartridge = _cartridge;
+}
